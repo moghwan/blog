9cfcd6fb76e9247a0fbe2a96a31ff0fd7b9fec14
moghwan <chaoui.mar2@gmail.com>
2023-09-23 20:07:23 +0100

    tweaking conf and assets

--- app.config.ts
+++ app.config.ts
@@ -1,41 +1,31 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
-    image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
-      width: 400,
-      height: 300
-    },
+    title: 'moghwan\'s blog',
+    // description: 'The minimalist blog theme',
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left',
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo.png', // path of the logo
+        pathDark: '/logo-dark.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'moghwan\'s blog' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        enabled: false,
       },
-      navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      navigation: false,
+      alignment: 'center',
+      message: ''
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'moghwan',
+      github: 'moghwan',
+      instagram: '_moghwan',
       linkedin: {
         icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://www.linkedin.com/in/moghwan'
       }
     },
-    form: {
-      successMessage: 'Message sent. Thank you!'
-    }
   }
 })
