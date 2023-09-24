22a9b5b3cb3b7cfc18e0978b4f23f061b2d87a83
moghwan <chaoui.mar2@gmail.com>
2023-09-24 12:58:32 +0100

    new image component

---
+++ components/content/Image.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+defineProps({
+  image: {
+    type: String,
+    default: null
+  },
+  imageAlt: {
+    type: String,
+    default: 'Hero Image'
+  },
+  imagePosition: {
+    type: String,
+    default: 'right'
+  }
+})
+</script>
+
+<template>
+  <section class="hero">
+    <div class="layout">
+      <NuxtImg
+        v-if="image"
+        :class="imagePosition"
+        :src="image"
+        :alt="imageAlt"
+        :width="16"
+        :height="9"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.hero': {
+    '.layout': {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
+      gap: '{space.8}',
+      '@lg': {
+        gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+      },
+      img: {
+        width: '100%',
+        //aspectRatio: '16 / 9',
+        objectFit: 'cover',
+        borderRadius: '{radii.md}',
+        '&.left': {
+          order: -1
+        }
+      },
+    }
+  }
+})
+</style>
