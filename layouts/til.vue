30730a73918d1e976d4058d91888e451d2c80708
moghwan <chaoui.mar2@gmail.com>
2025-06-11 21:58:19 +0100

    update base layout

--- layouts/til.vue
+++ layouts/til.vue
@@ -1,6 +1,27 @@
 <template>
   <main>
-    haaaaaaaaaaaaaaaaaaaaaaaaaaaaaa tils
-    <slot />
+
+    <div class="til-header">
+      <p class="til-title">Today I learned</p>
+      <p>Random cool stuffs I learned at random moments</p>
+    </div>
+
+    <slot/>
   </main>
 </template>
+
+<style scoped lang="ts">
+css({
+  '.til-header': {
+    minHeight: '20vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '.til-title': {
+      fontWeight: 'bold',
+      fontSize: '2rem',
+    }
+    },
+})
+</style>
